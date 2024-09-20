cf7d8a0bd8fd0aec513bbab62aa43f6aaf4e65ea
nazirumar <nazirumar888@gmail.com>
2024-09-20 14:20:28 +0100

    lesson

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,3 +1,4 @@
+import uuid
 import helpers
 from django.db import models
 from django.utils.text import slugify
@@ -7,23 +8,153 @@ helpers.cloudinary_init()
 
 class AccessRequirement(models.TextChoices):
     ANYONE = "any", "Anyone"
-    EMAIL_REQUIRED ='email_required', "Email required"
+    EMAIL_REQUIRED = "email", "Email required"
 
 class PublishStatus(models.TextChoices):
-    PUBLISHED = "pub", "Published"
+    PUBLISHED = "publish", "Published"
     COMING_SOON = "soon", "Coming Soon"
-    DRAFT = "drf", "Draft"
+    DRAFT = "draft", "Draft"
 
 
 def handle_upload(instance, filename):
-    return f"courses/{filename}"
+    return f"{filename}"
+
+# from courses.models import Course
+# Course.objects.all() -> list out all courses
+# Course.objects.first() -> first row of all courses
+
+def generate_public_id(instance, *args, **kwargs):
+    title = instance.title
+    unique_id = str(uuid.uuid4()).replace("-", "")
+    if not title:
+        return unique_id
+    slug = slugify(title)
+    unique_id_short = unique_id[:5]
+    return f"{slug}-{unique_id_short}"
+
+
+def get_public_id_prefix(instance, *args, **kwargs):
+    if hasattr(instance, 'path'):
+        path = instance.path
+        if path.startswith("/"):
+            path = path[1:]
+        if path.endswith('/'):
+            path = path[:-1]
+        return path
+    public_id = instance.public_id
+    model_class = instance.__class__
+    model_name = model_class.__name__
+    model_name_slug = slugify(model_name)
+    if not public_id:
+        return f"{model_name_slug}"
+    return f"{model_name_slug}/{public_id}"
+
+def get_display_name(instance, *args, **kwargs):
+    if hasattr(instance, 'get_display_name'):
+        return instance.get_display_name()
+    elif hasattr(instance, 'title'):
+        return instance.title
+    model_class = instance.__class__
+    model_name = model_class.__name__
+    return f"{model_name} Upload"
+
+# get_thumbnail_display_name = lambda instance: get_display_name(instance, is_thumbnail=True)
 
 class Course(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
-    # image = models.ImageField(upload_to=handle_upload, blank=True,  null=True)
-    image = CloudinaryField('image', null=True)
-    access = models.CharField(max_length=20, choices=AccessRequirement, default=AccessRequirement.EMAIL_REQUIRED)
-    status = models.CharField(max_length=10,
-                              choices=PublishStatus.choices,
-                              default=PublishStatus.DRAFT)
+    # uuid = models.UUIDField(default=uuid.uuid1, unique=True)
+    public_id = models.CharField(max_length=130, blank=True, null=True, db_index=True)
+    # image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
+    image = CloudinaryField(
+        "image", 
+        null=True, 
+        public_id_prefix=get_public_id_prefix,
+        display_name=get_display_name,
+        tags=["course", "thumbnail"]
+    )
+    access = models.CharField(
+        max_length=5, 
+        choices=AccessRequirement.choices,
+        default=AccessRequirement.EMAIL_REQUIRED
+    )
+    status = models.CharField(
+        max_length=10, 
+        choices=PublishStatus.choices,
+        default=PublishStatus.DRAFT
+        )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        # before save
+        if self.public_id == "" or self.public_id is None:
+            self.public_id = generate_public_id(self)
+        super().save(*args, **kwargs)
+        # after save
+
+    def get_absolute_url(self):
+        return self.path
+    
+    @property
+    def path(self):
+        return f"/courses/{self.public_id}"
+
+    def get_display_name(self):
+        return f"{self.title} - Course"
+
+    def get_thumbnail(self):
+        if not self.image:
+            return None
+        return helpers.get_cloudinary_image_object(
+            self, 
+            field_name='image',
+            as_html=False,
+            width=382
+        )
+
+    def get_display_image(self):
+        if not self.image:
+            return None
+        return helpers.get_cloudinary_image_object(
+            self, 
+            field_name='image',
+            as_html=False,
+            width=750
+        )
+
+    @property
+    def is_published(self):
+        return self.status == PublishStatus.PUBLISHED
+   
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    public_id = models.CharField(max_length=130, blank=True, null=True, db_index=True)
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    thumbnail = CloudinaryField("image", 
+                public_id_prefix=get_public_id_prefix,
+                display_name=get_display_name,
+                tags = ['thumbnail', 'lesson'],
+                blank=True, null=True)
+    video = CloudinaryField("video", 
+            public_id_prefix=get_public_id_prefix,
+            display_name=get_display_name,                
+            blank=True, 
+            null=True, 
+            type='private',
+            tags = ['video', 'lesson'],
+            resource_type='video')
+    order = models.IntegerField(default=0)
+    can_preview = models.BooleanField(default=False, help_text="If user does not have access to course, can they see this?")
+    status = models.CharField(
+        max_length=10, 
+        choices=PublishStatus.choices,
+        default=PublishStatus.PUBLISHED
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['order', '-updated']
